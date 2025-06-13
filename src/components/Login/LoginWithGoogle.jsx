5735dcb4d52b444d21260c271d0e66870da135cd
Ahmed Kamal <57872185+AhmedKamalMo@users.noreply.github.com>
2025-06-13 03:30:45 +0300

    s

--- src/components/Login/LoginWithGoogle.jsx
+++ src/components/Login/LoginWithGoogle.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { GoogleLogin } from '@react-oauth/google';
 import { useDispatch, useSelector } from 'react-redux';
-import { googleLoginThunk } from '../../services/Slice/Login/googleLogin';
+import { googleLoginThunk } from '../../services/Slice/login/googleLogin';
 import { emitter } from '../../features/emitter';
 import { toast } from 'react-toastify';
 
 const LoginWithGoogle = () => {
     const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.userProfile.profile) || [];
-  
+    const { user } = useSelector((state) => state.userProfile.profile) || [];
+
     const handleGoogleLoginSuccess = (credentialResponse) => {
-        const idToken = credentialResponse.credential; 
-        dispatch(googleLoginThunk({idToken:idToken,email:user?.email,name:user?.name}));  
+        const idToken = credentialResponse.credential;
+        dispatch(googleLoginThunk({ idToken: idToken, email: user?.email, name: user?.name }));
         emitter.emit('close-modal')
-      };
+    };
     const handleGoogleLoginFailure = (error) => {
         console.log(`Google Login Failed ${error}`);
         emitter.emit('open-modal')
         toast.error("This is an error message!", {
             position: "top-right",
-            autoClose: 3000, 
+            autoClose: 3000,
             hideProgressBar: false,
             closeOnClick: true,
-            pauseOnHover: true, 
-            draggable: true, 
-            theme: "light", 
-          });
-      };
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+        });
+    };
     return (
         <>
             <div className='mt-4'>
